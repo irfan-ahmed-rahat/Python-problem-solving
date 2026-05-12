345114cd80014061b3df9438bb6fe64758d65011
Irfan Ahmed Rahat <irfanahmedrahat.nxtphase@gmail.com>
2026-05-13 04:07:03 +0600

    Update main.py

--- 01_find_largest_number/main.py
+++ 01_find_largest_number/main.py
@@ -1,1 +1,15 @@
+# Take input from the user and convert to integer
+num_1 = int(input("Enter your first number: "))
+num_2 = int(input("Enter your second number: "))
 
+# Check if the first number is greater
+if num_1 > num_2:
+    print("First number is larger")
+
+# Check if both numbers are equal
+elif num_1 == num_2:
+    print("Both are equal")
+
+# If none of the above conditions are true, the second number must be larger
+else:
+    print("Second number is larger")
