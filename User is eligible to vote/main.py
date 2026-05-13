997c90738623413059340a2db10cf7c7bd0e06dd
Irfan Ahmed Rahat <irfanahmedrahat.nxtphase@gmail.com>
2026-05-14 01:32:14 +0600

    Update main.py

--- User is eligible to vote/main.py
+++ User is eligible to vote/main.py
@@ -1,1 +1,13 @@
+# Check if a user is eligible to vote based on age.
 
+
+# Take age input from the user and convert to integer
+age = int(input("Enter your age: "))
+
+# Check if age is 18 or greater
+if age >= 18:
+    print("You are eligible to vote")
+
+# If age is less than 18
+else:
+    print("You are not eligible to vote")
