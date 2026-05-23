b0d31f58c57d28457b4b96e789327d5551da49e8
Irfan Ahmed Rahat <irfanahmedrahat.nxtphase@gmail.com>
2026-05-23 21:19:38 +0600

    Create main.py

---
+++ 06_check_positive_negative_or_zero/main.py
@@ -0,0 +1,17 @@
+ Check whether a number is positive, negative, or zero.
+"""
+
+# Take a number as input from the user (using float to support decimals)
+num = float(input("Enter your number: "))
+
+# Check if the number is greater than zero
+if num > 0:
+    print(f"{num} is positive")
+
+# Check if the number is less than zero
+elif num < 0:
+    print(f"{num} is negative")
+
+# If it is neither greater nor less than zero, it must be zero
+else:
+    print("Number is 0")
