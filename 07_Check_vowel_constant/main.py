f6d623c3c0f278fce19ce7cb5fce8a2aa0cb35cd
Irfan Ahmed Rahat <irfanahmedrahat.nxtphase@gmail.com>
2026-06-22 01:07:46 +0600

    Update main.py

--- 07_Check_vowel_constant/main.py
+++ 07_Check_vowel_constant/main.py
@@ -1,1 +1,12 @@
+"""
+Problem 7: Check whether a character is a vowel or consonant.
+"""
 
+
+char = input("Enter your keyword: ")
+
+if char in "aeiouAEIOU":
+    print(f"{char} is a vowel")
+  
+else:
+    print(f"{char} is not a vowel")
