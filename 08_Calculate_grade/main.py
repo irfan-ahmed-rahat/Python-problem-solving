4484e7f23eab92a60ec1ea444617a69996bd5028
Irfan Ahmed Rahat <irfanahmedrahat.nxtphase@gmail.com>
2026-06-22 01:19:55 +0600

    Create main.py

---
+++ 08_Calculate_grade/main.py
@@ -0,0 +1,17 @@
+"""
+Problem 8: Take marks as input and assign a grade.
+"""
+
+marks = float(input("Enter your marks: "))
+
+if marks >= 80:
+    print("A")
+
+elif marks >= 70:
+    print("B")
+
+elif marks >= 60:
+    print("C") 
+
+else:
+    print("You Failed 😂")
