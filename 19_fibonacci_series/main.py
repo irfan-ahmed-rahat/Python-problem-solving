2e05549e92e2086145fe952fac40396ff7ece75b
Irfan Ahmed Rahat <irfanahmedrahat.nxtphase@gmail.com>
2026-08-12 13:52:56 +0600

    Update main.py

--- 19_fibonacci_series/main.py
+++ 19_fibonacci_series/main.py
@@ -12,7 +12,7 @@ num2 = 1
 print(f"\nFibonacci Series up to {n} terms:")
 
 # ধাপ ৩: লুপ 
-for i in range(1, n + 1):
+for i in range(1, n + 1): # range(num) / range (0, num+1) দিলেও হবে
     
     # ধাপ ৪: প্রথম সংখ্যাটি প্রিন্ট  
     print(num1, end=" ")
